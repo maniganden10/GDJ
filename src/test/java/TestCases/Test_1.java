b606695a555cd2e8c18812c0d1a6b17ab572e1f7
maniganden10 <maniganden10@gmail.com>
2025-04-04 12:40:30 +0530

    last commit

--- src/test/java/TestCases/Test_1.java
+++ src/test/java/TestCases/Test_1.java
@@ -5,6 +5,7 @@ import org.testng.AssertJUnit;
 
 import java.util.Properties;
 
+import org.openqa.selenium.devtools.v130.systeminfo.SystemInfo;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,6 +20,7 @@ public class Test_1 extends BasicSetup{
 		hp=new Homepage(driver,wait);
 		prop=super.prop;
 		boolean actualCity = hp.setCity(prop.getProperty("city"));
+		System.out.println("last push");
 		Assert.assertTrue(actualCity);
 	}
 	/*
